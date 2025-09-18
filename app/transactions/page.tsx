0f4a78cd9ece4200ce8df21d7e08e0da5e21aab5
DGTJS <joaodiegom15@gmail.com>
2025-09-17 23:42:48 -0300

    feat: list transactions from database

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,16 @@
-const Transactions = () => {
-  return <></>;
+import { db } from "../_lib/prisma";
+
+const Transactions = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div>
+      {transactions.map((transaction) => (
+        <div key={transaction.id}>
+          {transaction.name} {transaction.amount}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Transactions;
