813588f03f862d327c82a1c37bf63e7dc46a37b2
DGTJS <joaodiegom15@gmail.com>
2025-10-07 11:59:18 -0300

    feat: ensure transaction and subscriptions pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,10 +1,22 @@
 import AddTransactionButton from "../_components/add-transaction-button";
-import { DataTable } from "../_components/ui/data-table"; // Adjust the path if needed
+import { DataTable } from "../_components/ui/data-table";
 import { db } from "../_lib/prisma";
 import { transactionsColumns } from "./_columns/index";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Transactions = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+
+  const transactions = await db.transaction.findMany({
+    where: {
+      UserId: userId,
+    },
+  });
+
   return (
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
